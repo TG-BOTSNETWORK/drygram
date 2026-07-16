08d695044a0cd59bc9f97ad2364a55a2952afca2
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2026-07-16 16:32:15 +0530

    Delete tests/test_boosts.py

--- tests/test_boosts.py
+++
@@ -1,9 +0,0 @@
-# DryGram Developed By Santhu
-# mail: telegramsanthu@gmail.com
-import pytest
-from drygram import Chat
-
-def test_chat_boosts():
-    chat = Chat(id=-100123, type="channel")
-    assert chat.id == -100123
-    assert chat.type == "channel"
